5f91a536c8c72da68e92b51ecef911bf6109c5bc
baugh70 <zakkyzakkba@gmail.com>
2015-04-29 12:31:51 -0400

    Update Calculator.java

--- Calculator/src/us/legioncraft/calculator/Calculator.java
+++ Calculator/src/us/legioncraft/calculator/Calculator.java
@@ -1,1 +1,17 @@
+package us.legioncraft.calculator;
 
+public class Calculator {
+  private JFrame fame;
+  private JButton bClear;
+  private JButtin bX;
+  
+  public static void main(String[] args){
+    
+  }
+  
+  public void startFrame(){
+    frame = new JFrame();
+    
+    frame.setSize(400, 500);
+  }
+}
